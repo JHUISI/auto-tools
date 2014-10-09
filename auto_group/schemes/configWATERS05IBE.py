ffc1dda0e66866693e21a0eb7719c30170897097
Christina Garman <ChristinaLGarman@gmail.com>
2014-10-09 16:54:30 -0400

    adding SDL for Waters05 IBE scheme

---
+++ auto_group/schemes/configWATERS05IBE.py
@@ -0,0 +1,19 @@
+schemeType = "PKENC"
+short = "secret-keys"
+#short = "ciphertext"
+#operation = "exp"
+#short = "both"
+#dropFirst = "secret-keys"
+#short = "public-keys"
+
+setupFuncName = "setup"
+keygenFuncName = "keygen"
+encryptFuncName = "encrypt"
+decryptFuncName = "decrypt"
+
+masterPubVars = ["pk"]
+masterSecVars = ["msk"]
+
+keygenPubVar = "pk"
+keygenSecVar = "sk"
+ciphertextVar = "ct"
