b2ed33fb69faa41167737f3bcc632ce644c94345
Christina Garman <ChristinaLGarman@gmail.com>
2014-11-08 23:46:37 -0500

    edits to BGW05

--- auto_group/schemes/configBGW.py
+++ auto_group/schemes/configBGW.py
@@ -1,6 +1,10 @@
 schemeType = "PKENC"
-short = "secret-keys"
+assumption = ["lDBDHE"]
+reduction = ["reductionBGW05"]
+
+#short = "secret-keys"
 #short = "ciphertext"
+short = "public-keys"
 
 setupFuncName = "setup"
 keygenFuncName = "keygen"
@@ -13,3 +17,23 @@ masterSecVars = ["msk"]
 keygenPubVar = "pk"
 keygenSecVar  = "sk"
 ciphertextVar = "ct"
+
+assumpMasterPubVars = ["assumpVar"]
+assumpMasterSecVars = ["assumpKey"]
+
+assumpSetupFuncName = "setup"
+assumpFuncName = "assump"
+
+reducSetupFuncName = "setup"
+reducQueryFuncName = "queries"
+reducChallengeFuncName = "challenge"
+
+reducCiphertextVar = "ct"
+reducQueriesSecVar = "d"
+
+reducMasterPubVars = ["assumpVar", "reductionKey"]
+reducMasterSecVars = ["assumpKey", "reductionParams"]
+
+#maps assumption variables to reduction variables ie 'ct2':'C2' (assump:reduc)
+reductionMap = {'g':'g', 'g1':'g1', 'g2':'g2', 'ct1':'C1', 'ct2':'C2', 'ct3':'C3', 'd1':'d1', 'd2':'d2', 'A':'A', 'B':'B', 'C':'C', 'uprime':'ut'}
+
