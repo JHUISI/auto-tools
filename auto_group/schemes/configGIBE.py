1b9285f4f8650259e2b276ed76b73dc76472895a
Christina Garman <ChristinaLGarman@gmail.com>
2014-11-09 03:46:13 -0500

    preliminary Gentry 06 SDL

--- auto_group/schemes/configGIBE.py
+++ auto_group/schemes/configGIBE.py
@@ -1,7 +1,11 @@
 schemeType = "PKENC"
-short = "secret-keys"
+assumption = ["qDTABDHE"]
+reduction = ["reductionGENTRY06"]
+
+#short = "secret-keys"
 #short = "ciphertext"
 #short = "both"
+short = "public-keys"
 #operation = "exp"
 
 setupFuncName = "setup"
@@ -10,8 +14,28 @@ encryptFuncName = "encrypt"
 decryptFuncName = "decrypt"
 
 masterPubVars = ["pk"]
-masterSecVars = ["mk"]
+masterSecVars = ["msk"]
 
 keygenPubVar = "pk"
 keygenSecVar = "sk"
 ciphertextVar = "ct"
+
+assumpMasterPubVars = ["assumpVar"]
+assumpMasterSecVars = ["assumpKey"]
+
+assumpSetupFuncName = "setup"
+assumpFuncName = "assump"
+
+reducSetupFuncName = "setup"
+reducQueryFuncName = "queries"
+reducChallengeFuncName = "challenge"
+
+reducCiphertextVar = "ct"
+reducQueriesSecVar = "d"
+
+reducMasterPubVars = ["assumpVar", "reductionKey"]
+reducMasterSecVars = ["assumpKey", "reductionParams"]
+
+#maps assumption variables to reduction variables ie 'ct2':'C2' (assump:reduc)
+reductionMap = {'g':'g', 'g1':'g1', 'g2':'g2', 'ct1':'C1', 'ct2':'C2', 'ct3':'C3', 'd1':'d1', 'd2':'d2', 'A':'A', 'B':'B', 'C':'C', 'uprime':'ut'}
+
