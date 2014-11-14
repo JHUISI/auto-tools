4ea21280ee147229cf1afde88350e3f46f8b28a6
Christina Garman <ChristinaLGarman@gmail.com>
2014-11-13 23:49:04 -0500

    benchmarking script for AutoGroup+

---
+++ auto_group/benchSchemesAutoGroupPlus.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Running Waters09 DSE experiment"
+for i in {1..10}
+do
+    python3 runAutoGroup.py --sdl schemes/dse09.sdl --config schemes/configWATERS09.py -o TimingWaters09.txt -b
+done
+
+echo "Running Waters05 DSE experiment"
+for i in {1..10}
+do
+    python3 runAutoGroup.py --sdl schemes/waters05IBE.sdl --config schemes/configWATERS05IBE.py -o TimingWaters05.txt -b
+done
+
+echo "Running GentryIBE experiment"
+for i in {1..10}
+do
+    python3 runAutoGroup.py --sdl schemes/gentry06.sdl --config schemes/configGIBE.py -o TimingGentry06.txt -b
+done
+
+echo "Running BB04IBE experiment"
+for i in {1..10}
+do
+    python3 runAutoGroup.py --sdl schemes/BB04IBE.sdl --config schemes/configBB04IBE.py -o TimingBB04IBE.txt -v
+done
+
+echo "Running BGW05 experiment"
+for i in {1..10}
+do
+    python3 runAutoGroup.py --sdl schemes/bgw05.sdl --config schemes/configBGW.py -o TimingBGW05.txt -v
+done
