b619f10149a84b89f67fb76b42e2537970776f36
Christina Garman <ChristinaLGarman@gmail.com>
2014-11-14 01:10:06 -0500

    benchmarking files for AutoGroup+

--- auto_group/benchSchemesAutoGroupPlus.sh
+++ auto_group/benchSchemesAutoGroupPlus.sh
@@ -21,11 +21,11 @@ done
 echo "Running BB04IBE experiment"
 for i in {1..10}
 do
-    python3 runAutoGroup.py --sdl schemes/BB04IBE.sdl --config schemes/configBB04IBE.py -o TimingBB04IBE.txt -v
+    python3 runAutoGroup.py --sdl schemes/BB04IBE.sdl --config schemes/configBB04IBE.py -o TimingBB04IBE.txt -b
 done
 
 echo "Running BGW05 experiment"
 for i in {1..10}
 do
-    python3 runAutoGroup.py --sdl schemes/bgw05.sdl --config schemes/configBGW.py -o TimingBGW05.txt -v
+    python3 runAutoGroup.py --sdl schemes/bgw05.sdl --config schemes/configBGW.py -o TimingBGW05.txt -b
 done
