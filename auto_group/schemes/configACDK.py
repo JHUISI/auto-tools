72a2ccd3fcdd5b9d50b38bdbffee81230ab56d8a
Christina Garman <ChristinaLGarman@gmail.com>
2014-11-25 17:11:39 -0500

    adding ACDKNO reductions/new config file

--- auto_group/schemes/configACDK.py
+++ auto_group/schemes/configACDK.py
@@ -1,8 +1,12 @@
 schemeType = "PKSIG"
+assumption = ["DLIN-ACDKNO", "CDH-ACDKNO"]
+reduction = ["reductionACDK12Lemma6", "reductionACDK12Lemma7", "reductionACDK12Lemma8"]
+
 #short = "public_keys"
-#short = "signature"
-short = "both"
-operation = "exp"
+short = "signature"
+#short = "assumption"
+#short = "both"
+#operation = "exp"
 
 setupFuncName 	= "setup"
 keygenFuncName 	= "keygen"
@@ -15,4 +19,24 @@ masterSecVars = ["sk"]
 keygenPubVar = "vk"
 keygenSecVar = "sk" 
 signatureVar = "sig" 
+
+assumpMasterPubVars = ["assumpVar"]
+assumpMasterSecVars = ["assumpKey"]
+
+assumpSetupFuncName = "setup"
+assumpFuncName = "assump"
+
+reducSetupFuncName = "setup"
+reducQueryFuncName = "queries"
+#reducChallengeFuncName = "challenge"
+
+reducCiphertextVar = "sig"
+reducQueriesSecVar = ""
+
+reducMasterPubVars = ["assumpVar", "reductionKey"]
+reducMasterSecVars = ["assumpKey"]
+
+#maps assumption variables to reduction variables ie 'ct2':'C2' (assump:reduc)
+reductionMap = {'g':'g', 'g1':'g1', 'g2':'g2', 'ct1':'C1', 'ct2':'C2', 'ct3':'C3', 'd1':'d1', 'd2':'d2', 'A':'A', 'B':'B', 'C':'C', 'uprime':'ut'}
+
 
