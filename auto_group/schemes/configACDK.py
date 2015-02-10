0924b943fecf0cf83b571e00c11330c2146d2fb1
Christina Garman <ChristinaLGarman@gmail.com>
2015-02-10 01:35:40 -0500

    updates to ACDKNO config file

--- auto_group/schemes/configACDK.py
+++ auto_group/schemes/configACDK.py
@@ -2,7 +2,7 @@ schemeType = "PKSIG"
 assumption = ["DLIN-ACDKNO", "CDH-ACDKNO"]
 reduction = ["reductionACDK12Lemma6", "reductionACDK12Lemma7", "reductionACDK12Lemma8"]
 
-short = "public_keys"
+short = "public-keys"
 #short = "signature"
 #short = "assumption"
 #short = "both"
@@ -39,4 +39,5 @@ reducMasterSecVars = ["assumpKey"]
 #maps assumption variables to reduction variables ie 'ct2':'C2' (assump:reduc)
 reductionMap = {'g':'g', 'g1':'g1', 'g2':'g2', 'ct1':'C1', 'ct2':'C2', 'ct3':'C3', 'd1':'d1', 'd2':'d2', 'A':'A', 'B':'B', 'C':'C', 'uprime':'ut'}
 
+assumption_reduction_map = { "reductionACDK12Lemma6" : "DLIN-ACDKNO", "reductionACDK12Lemma7" : "DLIN-ACDKNO", "reductionACDK12Lemma8" : "CDH-ACDKNO" }
 
