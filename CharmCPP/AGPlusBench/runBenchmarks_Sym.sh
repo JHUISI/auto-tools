602fc1db918b9d1bb6a61b57a5fc1d6964cb03bf
Christina Garman <ChristinaLGarman@gmail.com>
2015-02-20 03:11:54 -0500

    updates to AutoGroupPlus timing benchmark script

--- CharmCPP/AGPlusBench/runBenchmarks_Sym.sh
+++ CharmCPP/AGPlusBench/runBenchmarks_Sym.sh
@@ -15,3 +15,13 @@ echo -e "\nBB04 Asym Assumption\n"
 
 echo -e "\nACDKNO Sym\n"
 ./TestACDKNOSym 100 10 fixed
+
+echo -e "\nACDKNO Asym PK\n"
+./TestACDKNO_pk_test 100 10 fixed
+
+echo -e "\nACDKNO Asym Sig\n"
+./TestACDKNO_sig_test 100 10 fixed
+
+echo -e "\nACDKNO Asym Assumption\n"
+./TestACDKNO_assump_test 100 10 fixed
+
